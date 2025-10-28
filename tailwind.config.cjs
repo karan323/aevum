d48252d78f5e4f208fdca9613a641594097b9593
KARAN323 <52529497+karan323@users.noreply.github.com>
2025-10-28 15:36:37 -0400

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        aevumGold: '#b8862b',
+        aevumNavy: '#0b2740'
+      }
+    }
+  },
+  plugins: []
+}
